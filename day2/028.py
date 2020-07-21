d1f3799347325c8fd96c9d09f0c1838c4de99457
root <root@centos6.8>
2020-07-21 11:26:19 +0900

    Modified 028.py

--- day2/028.py
+++ day2/028.py
@@ -20,9 +20,15 @@ def comp2(seq: str) -> str:
         comp += d_comp[s]
     return comp
 
-seq = sys.argv[1] #ATGTTATAG
-c1 = comp1(seq)
-c2 = comp2(seq)
-print(seq)
-print(c1)
-print(c2)
+#what's under here doesn't print when this file is imported
+if __name__=="__main__":
+    if len(sys.argv) != 2:
+        print(f"#usage: python {sys.argv[0]} [string]")
+        sys.exit()
+
+    seq = sys.argv[1] #ATGTTATAG
+    c1 = comp1(seq)
+    c2 = comp2(seq)
+    print(seq)
+    print(c1)
+    print(c2)
