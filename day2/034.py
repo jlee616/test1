df7c754cf5864b3e3232458ca83972d360c6544e
root <root@centos6.8>
2020-07-21 11:33:49 +0900

    Day2 materials

---
+++ day2/034.py
@@ -0,0 +1,13 @@
+l = [3,1,1,2,0,0,2,3,3]
+
+max_val = l[0]
+min_val = l[0]
+
+for i in l:
+    if i > max_val:
+        max_val = i
+    if i < min_val:
+        min_val = i
+
+print(f"max: {max_val}")
+print(f"min: {min_val}")
